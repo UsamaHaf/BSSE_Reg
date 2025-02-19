5a37e7d4fa86db83e39c92a5ce52268a39480a91
mhmd.usama.hafeez <mhmd.usama.hafeez@gmail.com>
2025-02-19 09:32:28 +0500

    Login Form Validations

--- app/src/main/java/com/usama/uos/bsse_reg/LoginForm.kt
+++ app/src/main/java/com/usama/uos/bsse_reg/LoginForm.kt
@@ -1,19 +1,52 @@
 package com.usama.uos.bsse_reg
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LoginForm : AppCompatActivity() {
+
+   private lateinit var edtLoginEmail: EditText
+   private lateinit var edtLoginPassword: EditText
+   private lateinit var btnLogin: Button
+
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       setContentView(R.layout.activity_login_form)
-      ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-         val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-         v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-         insets
+
+      edtLoginEmail = findViewById(R.id.loginEmail)
+      edtLoginPassword = findViewById(R.id.loginPassword)
+      btnLogin = findViewById(R.id.btnLogin)
+
+      btnLogin.setOnClickListener {
+
+         val strEmail = edtLoginEmail.text.toString()
+         val strPassword = edtLoginPassword.text.toString()
+
+         loginUser(strEmail, strPassword)
       }
+
+
+   }
+
+   private fun loginUser(strEmail: String, strPassword: String) {
+
+      if (strEmail.isEmpty()) {
+         Toast.makeText(this@LoginForm, "Please Enter EmailAddress", Toast.LENGTH_SHORT).show()
+      } else if (strPassword.isEmpty()) {
+         Toast.makeText(this@LoginForm, "Enter password", Toast.LENGTH_SHORT).show()
+      } else {
+
+         Log.e("Email" , "Email:- $strEmail")
+         Log.e("password" , "Password:- $strPassword")
+
+         startActivity(Intent(this@LoginForm, IntentsActivity::class.java))
+
+      }
+
    }
 }
