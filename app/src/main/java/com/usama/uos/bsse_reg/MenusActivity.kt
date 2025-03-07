99c2b9c2dcf553e62c89312407483a6585074f3c
mhmd.usama.hafeez <mhmd.usama.hafeez@gmail.com>
2025-03-07 10:57:20 +0500

    Pickers

--- app/src/main/java/com/usama/uos/bsse_reg/MenusActivity.kt
+++ app/src/main/java/com/usama/uos/bsse_reg/MenusActivity.kt
@@ -1,28 +1,77 @@
 package com.usama.uos.bsse_reg
 
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.DatePicker
 import android.widget.PopupMenu
 import android.widget.TextView
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MenusActivity : AppCompatActivity() {
+
+   lateinit var datePicker: DatePicker
+   lateinit var getSelectedDate: TextView
+   lateinit var getSelectedTime: TextView
+   lateinit var timePicker: TimePicker
+
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       setContentView(R.layout.activity_menus)
 
+      datePicker = findViewById(R.id.datePicker)
+      timePicker = findViewById(R.id.timePicker)
+      getSelectedDate = findViewById(R.id.getSelectedDate)
+      getSelectedTime = findViewById(R.id.getSelectedTime)
+
       val btnContextMenuShow = findViewById<TextView>(R.id.btnContextMenu)/*Context Menu*/
       registerForContextMenu(btnContextMenuShow)
-
       val btnPopUpMenuShow = findViewById<TextView>(R.id.btnPopUPMenu)/*PopUp Menu*/
       btnPopUpMenuShow.setOnClickListener {
          showPopUpMenu(btnPopUpMenuShow)
       }
+
+      val today = Calendar.getInstance()
+      datePicker.init(today.get(Calendar.YEAR),
+      today.get(Calendar.MONTH),
+      today.get(Calendar.DAY_OF_MONTH)){ view , year, month, day ->
+
+         val date = "You selected: $day/${month + 1}/$year"
+         getSelectedDate.text = date
+
+      }
+
+      timePicker.setOnTimeChangedListener { timePicker, hours, minutes ->
+         var hours1 = hours
+         var amPm = ""
+         when{ hours1 == 0 -> {
+            hours1 += 12
+            amPm = "AM"
+         }
+            hours1 == 12 -> amPm ="PM"
+            hours1 > 12 ->{
+               hours1 -= 12
+               amPm = "PM"
+            }else -> amPm = "AM"
+
+         }
+         val hours2 = if (hours1 <10) "0$hours1" else hours1
+         val min = if(minutes <10) "0$minutes" else minutes
+
+         val time = "Time is: $hours2 : $min $amPm"
+
+         getSelectedTime.text = time
+
+
+      }
+
+
 
    }
 
@@ -52,29 +101,23 @@ class MenusActivity : AppCompatActivity() {
       super.onCreateContextMenu(menu, v, menuInfo)
       menuInflater.inflate(R.menu.context_menu, menu)
    }
-
    override fun onContextItemSelected(item: MenuItem): Boolean {
       return when (item.itemId) {
          R.id.deleteMenu -> {
             Toast.makeText(this@MenusActivity, "Delete Meun", Toast.LENGTH_SHORT).show()
             true
          }
-
          R.id.editMenu -> {
             Toast.makeText(this@MenusActivity, "Edit Meu", Toast.LENGTH_SHORT).show()
             true
          }
-
          R.id.adminMenu -> {
             Toast.makeText(this@MenusActivity, "Make Admin Meu", Toast.LENGTH_SHORT).show()
             true
          }
-
          else -> super.onContextItemSelected(item)
       }
    }
-
-
 
    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
       menuInflater.inflate(R.menu.option_menu, menu)
