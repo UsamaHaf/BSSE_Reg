c2704eb436d2d6413b546f428d1b055fdec1b75f
mhmd.usama.hafeez <mhmd.usama.hafeez@gmail.com>
2025-05-02 12:30:57 +0500

    Firebase Create User

--- app/src/main/java/com/usama/uos/bsse_reg/SignUpForm.kt
+++ app/src/main/java/com/usama/uos/bsse_reg/SignUpForm.kt
@@ -1,21 +1,25 @@
 package com.usama.uos.bsse_reg
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpForm : AppCompatActivity() {
 
+   private lateinit var firebaseAuth: FirebaseAuth
+
    override fun onCreate(savedInstanceState: Bundle?) {
+
       super.onCreate(savedInstanceState)
       setContentView(R.layout.activity_sign_up_form)
+
+      firebaseAuth = FirebaseAuth.getInstance()
+
 
       val btnSignUpForm = findViewById<Button>(R.id.btnSignup)
       val edtEmail = findViewById<EditText>(R.id.edtEmailAddress)
@@ -27,6 +31,7 @@ class SignUpForm : AppCompatActivity() {
 
          if (edtEmail.text.toString().isEmpty()) {
             Toast.makeText(this@SignUpForm, "Please Enter EmailAddress", Toast.LENGTH_SHORT).show()
+
          } else if (edtFirstName.text.toString().isEmpty()) {
             Toast.makeText(this@SignUpForm, "Please Enter FirstName", Toast.LENGTH_SHORT).show()
 
@@ -35,12 +40,44 @@ class SignUpForm : AppCompatActivity() {
 
          } else if (edtPassword.text.toString().isEmpty()) {
             Toast.makeText(this@SignUpForm, "Please Enter Password", Toast.LENGTH_SHORT).show()
+
          } else {
-            Toast.makeText(this@SignUpForm, "Please Enter Password", Toast.LENGTH_SHORT).show()
-            startActivity(Intent(this@SignUpForm, LoginForm::class.java))
+            val strEmail = edtEmail.text.toString()
+            val strPassword = edtPassword.text.toString()
+
+
+            signUpFirebaseUser(edtEmail.text.toString(), edtPassword.text.toString())
+            Log.e("UserEmail" , edtEmail.text.toString())
+            Log.e("UserPassword" , edtPassword.text.toString())
+
+
+
 
          }
       }
+
+   }
+
+
+   private fun signUpFirebaseUser(strEmail: String, strPassword: String) {
+
+      firebaseAuth.createUserWithEmailAndPassword(strEmail, strPassword).addOnCompleteListener { task ->
+
+         if (task.isSuccessful) {
+            Toast.makeText(this@SignUpForm, "SignIn Successfully", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this@SignUpForm, LoginForm::class.java))
+         }else{
+
+            Toast.makeText(this@SignUpForm, "SignIn Failed:- ${task.exception}", Toast.LENGTH_SHORT).show()
+
+            Log.e("FailedFirebase" , task.exception.toString())
+
+            startActivity(Intent(this@SignUpForm, LoginForm::class.java))
+         }
+
+
+      }
+
 
    }
 
--- app/src/main/java/com/usama/uos/bsse_reg/SplashScreenActivity.kt
+++ app/src/main/java/com/usama/uos/bsse_reg/SplashScreenActivity.kt
@@ -5,33 +5,39 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 import com.usama.uos.bsse_reg.SharedPref.MySharedPreferences
 
 class SplashScreenActivity : AppCompatActivity() {
 
    lateinit var sharedPreferences: MySharedPreferences
+   lateinit var firebaseAuth: FirebaseAuth
 
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       setContentView(R.layout.activity_splash_screen)
 
+      firebaseAuth = FirebaseAuth.getInstance()
+      FirebaseApp.initializeApp(this@SplashScreenActivity)
+
       sharedPreferences = MySharedPreferences(this@SplashScreenActivity)
 
-      /*Handler(Looper.getMainLooper()).postDelayed(Runnable {
-         startActivity(Intent(this, IntentsActivity::class.java))
-      },1000)*/
+      Handler(Looper.getMainLooper()).postDelayed(Runnable {
+         startActivity(Intent(this, SignUpForm::class.java))
+      },1000)
 
-      Handler().postDelayed(Runnable {
+      /*Handler().postDelayed(Runnable {
 
-         if(sharedPreferences.getLoginStatus("UserLoginStatus") == "True"){
+         if (sharedPreferences.getLoginStatus("UserLoginStatus") == "True") {
             startActivity(Intent(this, HomePageActivity::class.java))
 
-         }else{
+         } else {
             startActivity(Intent(this, LoginForm::class.java))
          }
 
 
          //startActivity(Intent(this, RecyclerViewActivity::class.java))
-      }, 1000)
+      }, 1000)*/
    }
 }
